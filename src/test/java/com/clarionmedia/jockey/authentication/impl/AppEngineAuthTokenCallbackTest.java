99f9eaa0d21f6942df5b39a2ec38ff3cc08820f6
Tyler Treat <ttreat31@gmail.com>
2013-08-10 17:17:45 -0500

    Add test stub method

--- src/test/java/com/clarionmedia/jockey/authentication/impl/AppEngineAuthTokenCallbackTest.java
+++ src/test/java/com/clarionmedia/jockey/authentication/impl/AppEngineAuthTokenCallbackTest.java
@@ -17,11 +17,15 @@
 package com.clarionmedia.jockey.authentication.impl;
 
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(RobolectricTestRunner.class)
 public class AppEngineAuthTokenCallbackTest {
 
+    @Test
+    public void testStub() {
 
+    }
 
 }
